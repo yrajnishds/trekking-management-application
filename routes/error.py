37db166a2e81943e8a951ac0ffe50c103f7d98bb
Rajnish Yadav <25f2004238@ds.study.iitm.ac.in>
2026-06-16 03:20:33 +0530

    fix: error route

--- routes/error.py
+++ routes/error.py
@@ -2,11 +2,11 @@ from flask import Blueprint, render_template
 
 error_bp = Blueprint('error', __name__)
 
-error_bp.app_errorhandler(404)
+@error_bp.app_errorhandler(404)
 def page_not_found(error):
     return render_template('error/500.html'), 404
 
 
-error_bp.app_errorhandler(500)
+@   error_bp.app_errorhandler(500)
 def server_not_found(error):
     return render_template('error/500.html'), 500
