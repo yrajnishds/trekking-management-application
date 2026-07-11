fd1cc1ce763af74c3cb2192071139a009959477d
Rajnish Yadav <25f2004238@ds.study.iitm.ac.in>
2026-07-11 17:11:51 +0530

    fix: error redirect

--- routes/error.py
+++ routes/error.py
@@ -1,24 +1,20 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, session
-from flask_login import logout_user
+from flask_login import logout_user, current_user
 
 error_bp = Blueprint('error', __name__)
 
 @error_bp.app_errorhandler(404)
 def page_not_found(error):
-    return render_template('error/404.html'), 404
+    return render_template('error/404.html', page = '404 - error'), 404
 
 
 @error_bp.app_errorhandler(500)
-def server_not_found(error):
-    return render_template('error/500.html'), 500
+def page_not_found(error):
+    return render_template('error/500.html', page = '500 - error'), 500
+
+
 
 @error_bp.app_errorhandler(403)
 def forbidden(error):
 
-    logout_user()
-    session.clear()
-    flash(
-        "Your session was terminated because you tried to access an unauthorized page.",
-        "danger"
-    )
-    return render_template('error/403.html'), 403
+    return render_template('error/403.html', page = '403 - Error'), 403
